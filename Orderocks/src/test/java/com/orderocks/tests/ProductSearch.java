043ba2c884f7aa5ffdbd155caaeabb13c82caabe
Shrinivas <shrinivasthorate2905@gmail.com>
2023-02-20 12:46:59 +0530

    Hii

--- Orderocks/src/test/java/com/orderocks/tests/ProductSearch.java
+++ Orderocks/src/test/java/com/orderocks/tests/ProductSearch.java
@@ -35,7 +35,7 @@ public class ProductSearch extends BaseClass {
 				.searchProduct("DEEP FROZEN KESAR MANGO PULP 12OZ").clickSearchIcon();
 		//Assert.assertEquals(home.getproduct2(),"Pepsi 2.25 L");
 	}
-
+	//LoginPage login = new LoginPage(driver);
 	@AfterTest
 	public void tearDown() {
 		driver.close();
