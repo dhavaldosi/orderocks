c160317382ad8ecd1c54aac1ec375ad51e58d379
Dhaval <dosi.dhaval@gmail.com>
2023-02-22 14:13:37 +0530

    Scroll and click

--- Orderocks/src/test/java/com/orderocks/tests/CancelOrder.java
+++ Orderocks/src/test/java/com/orderocks/tests/CancelOrder.java
@@ -3,7 +3,6 @@ package com.orderocks.tests;
 import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
